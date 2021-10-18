b0992e25dc21133c55b7b8d5c8af34bb0effcb63
LHM777 <777harvey777@gmail.com>
2021-10-18 23:40:59 +0800

    MainActivity.java

---
+++ app/src/main/java/com/example/saveimage2021/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.saveimage2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView ivBackGround;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivBackGround = findViewById(R.id.imageView);
+        btnSave = findViewById(R.id.btnSaveImage);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) ivBackGround.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                saveImageToGallery(bitmap);
+            }
+        });
+
+
+    }
+
+
+    private void saveImageToGallery(Bitmap bitmap){
+
+        OutputStream fos;
+
+        try{
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+
+                ContentResolver resolver = getContentResolver();
+                ContentValues contentValues =  new ContentValues();
+                contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "Image_" + ".jpg");
+                contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+                contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + File.separator + "TestFolder");
+                Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+                fos = resolver.openOutputStream(Objects.requireNonNull(imageUri));
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                Objects.requireNonNull(fos);
+
+                Toast.makeText(this, "Image Saved", Toast.LENGTH_SHORT).show();
+            }
+
+        }catch(Exception e){
+
+            Toast.makeText(this, "Image not saved \n" + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+}
